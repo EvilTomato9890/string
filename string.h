31ef52fa76fcdfcbc42eb815cc055ceaf17359ae
ScaryEvilTomato <omglol9898719@gmail.com>
2025-09-09 19:15:07 +0300

    init string.h

--- string.h
+++ string.h
@@ -1,1 +1,8 @@
-
+const char* strchr(const char* str, char ch);
+int puts(const char* str);
+size_t strlen(const char* str);
+char* strcpy(char* destin, const char* source);
+char* strncpy(char* destin, const char* source, size_t num );
+char* strcat (char* destination, const char* source);
+char* strncat(char* destin, const char* source, size_t num);
+ssize_t Getline(char **lineptr, size_t *n, FILE *stream);
